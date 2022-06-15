a52f2e9ef012041ea118f414016d1f91766688bd
SvetlanaGIP <gip1@acn.su>
2022-06-15 14:53:56 +1000

    Добавьте файлы проекта.

---
+++ HW23/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace HW23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            FactorialAsync(n);
+
+            Console.ReadKey();
+        }
+
+        static void Factorial(int n)
+        {
+            int s = 1;
+            for (int i = 1; i < n; i++)
+            {
+                s *= i;
+                Thread.Sleep(10);
+            }
+            Console.WriteLine(s);
+        }
+
+        static async void FactorialAsync(int n)
+        {
+            await Task.Run(() => Factorial(n));
+        }
+
+    }
+}
+
